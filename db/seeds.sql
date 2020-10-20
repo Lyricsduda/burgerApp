16fe01d07226267de96c8f052e812cb47bf85f3c
LyricDuda <Lyricsduda@gmail.com>
2020-10-20 16:22:54 -0400

    Intial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Cat Burger');
+INSERT INTO burgers (name) VALUES ('Mc Double');
---
+++ models/burger.js
@@ -0,0 +1,28 @@
+
+var orm = require("../config/orm.js");
+
+var Burger = {
+  all: function (cb) {
+    orm.all("Burgers", function (res) {
+      cb(res);
+    });
+  },
+
+  create: function (cols, vals, cb) {
+    orm.create("Burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("Burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+  delete: function (condition, cb) {
+    orm.delete("Burgers", condition, function (res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = Burger;
